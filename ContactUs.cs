ecd0bd2307558760b7b984770666dc1160c42783
Alejandro Franco <afranco7@gmail.com>
2019-03-01 14:44:29 -0500

    this is the automation practice

---
+++ ContactUs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace automationpractice
+{
+    [TestClass]
+    public class ContactUs
+    {
+        IWebDriver driver;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void ContactUsTest()
+        {
+            driver.Url = "http://automationpractice.com/index.php";
+            driver.FindElement(By.Id("contact-link")).Click();
+            new SelectElement(driver.FindElement(By.Id("id_contact"))).SelectByValue("2");
+            driver.FindElement(By.Id("email")).SendKeys("hola@mailinator.com");
+            driver.FindElement(By.Id("id_order")).SendKeys("1012252");
+
+            driver.FindElement(By.Id("message")).SendKeys("this is a test for automation");
+
+            driver.FindElement(By.Id("submitMessage")).Click();
+            string sucess = driver.FindElement(By.XPath("//*[@id=\"center_column\"]/p")).Text;
+
+            Assert.AreEqual("Your message has been successfully sent to our team.", sucess);
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("automationpractice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("automationpractice")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f19f34d-e906-4440-89a0-ad28664b1741")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
